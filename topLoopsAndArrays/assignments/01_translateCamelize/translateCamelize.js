f576b1aef72ebbf86ce06e75f0dcc4c88386e403
iKiray <exkiray@gmail.com>
2025-11-14 01:00:33 -0500

    Adjusted variable naming and added pseudo code

--- topLoopsAndArrays/assignments/01_translateCamelize/translateCamelize.js
+++ topLoopsAndArrays/assignments/01_translateCamelize/translateCamelize.js
@@ -1,6 +1,11 @@
 function camelize(str) {
-    const splitWords = str.split("-");
-    console.log(splitWords);
+    const splitArray = str.split("-");
+    /* pseudo code
+    create new array where first word in array is lower case
+    the follow words in the same array should be capitalized
+    e.g charAt(0) + slice(1)
+    */
+    console.log(splitArray);
 }
 
 camelize("background-color") == 'backgroundColor';
