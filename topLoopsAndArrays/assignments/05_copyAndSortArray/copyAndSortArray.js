5658a9d273538b9a39b94237ac4d60d69492fb21
iKiray <exkiray@gmail.com>
2025-11-15 03:28:11 -0500

    Added website's solution on file

--- topLoopsAndArrays/assignments/05_copyAndSortArray/copyAndSortArray.js
+++ topLoopsAndArrays/assignments/05_copyAndSortArray/copyAndSortArray.js
@@ -10,3 +10,16 @@ function copySorted(arr) {
 
 console.log( sorted ); // CSS, HTML, JavaScript
 console.log( arr ); // HTML, JavaScript, CSS (no changes)
+
+/*solution on website
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted );
+alert( arr );
+*/
