05f5beafe32d5f1f7a00374c0ef424c571f0bd19
iKiray <exkiray@gmail.com>
2025-11-15 02:17:12 -0500

    Finished my interpretation of the assignment

--- topLoopsAndArrays/assignments/01_translateCamelize/translateCamelize.js
+++ topLoopsAndArrays/assignments/01_translateCamelize/translateCamelize.js
@@ -1,12 +1,20 @@
 function camelize(str) {
-    const splitArray = str.split("-");
-    const firstWord = splitArray[0].toLowerCase();
-    /* pseudo code
-    create new array where first word in array is lower case
-    the follow words in the same array should be capitalized
-    e.g charAt(0) + slice(1)
-    */
-    console.log(firstWord);
+    const splitArray = str.split("-"); 
+    //split string into multiple words seperated by "-"
+    
+    const firstWord = splitArray[0].toLowerCase(); 
+    //first word index 0 all to lowercase
+    
+    const restWords = splitArray.slice(1);
+    //split the rest of the array starting at index 1
+    const restWordsCap = restWords.map(
+        word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    );
+    //every word first letter capitalized and the rest lower cased
+    
+    const joinWords = firstWord + restWordsCap.join("");
+    //join the words together to finally camelize
+    console.log(joinWords);
 }
 
 camelize("background-color") == 'backgroundColor';
