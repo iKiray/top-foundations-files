55d0c3b82331a447774cdd5bc21b88c45cd45213
iKiray <exkiray@gmail.com>
2025-11-15 01:41:38 -0500

    Added first word in array to lower case

--- topLoopsAndArrays/assignments/01_translateCamelize/translateCamelize.js
+++ topLoopsAndArrays/assignments/01_translateCamelize/translateCamelize.js
@@ -1,11 +1,12 @@
 function camelize(str) {
     const splitArray = str.split("-");
+    const firstWord = splitArray[0].toLowerCase();
     /* pseudo code
     create new array where first word in array is lower case
     the follow words in the same array should be capitalized
     e.g charAt(0) + slice(1)
     */
-    console.log(splitArray);
+    console.log(firstWord);
 }
 
 camelize("background-color") == 'backgroundColor';
