487af8b252d8b7579271b680386266a130cf344d
iKiray <exkiray@gmail.com>
2025-11-17 04:40:08 -0500

    Finished filterUniqueArrayMembers assignment

--- topLoopsAndArrays/assignments/07_filterUniqueArrayMembers/filterUniqueArrayMembers.js
+++ topLoopsAndArrays/assignments/07_filterUniqueArrayMembers/filterUniqueArrayMembers.js
@@ -1,9 +1,27 @@
 function unique(arr) {
   /* your code */
+  const uniqueSet = new Set(arr);
+  const uniqueArr = [...uniqueSet];
+  return uniqueArr;
+
+  /* could be written in function wrapper
+
+  const getUnique = (arr) => [...newSet(arr)];
+  */
 }
 
 let strings = ["Hare", "Krishna", "Hare", "Krishna",
   "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ];
 
-alert( unique(strings) ); // Hare, Krishna, :-O
+console.log( unique(strings) ); // Hare, Krishna, :-O
+
+/* this would be the other method taught traditionally */
+function unique2(arr) {
+  const uniqueArr = arr.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+  })
+  return uniqueArr;
+}
+
+console.log(unique2(strings));
