92feaee8010582cfe424e457c9f446eb05409606
iKiray <exkiray@gmail.com>
2025-11-17 04:28:24 -0500

    Finished shuffleAnArray assignment

--- topLoopsAndArrays/assignments/06_shuffleAnArray/shuffleAnArray.js
+++ topLoopsAndArrays/assignments/06_shuffleAnArray/shuffleAnArray.js
@@ -1,11 +1,20 @@
 let arr = [1, 2, 3];
 
-shuffle(arr);
+function shuffle(array) {
+    for(let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+console.log(shuffle(arr));
 // arr = [3, 2, 1]
 
-shuffle(arr);
+console.log(shuffle(arr));
 // arr = [2, 1, 3]
 
-shuffle(arr);
+console.log(shuffle(arr));
 // arr = [3, 1, 2]
 // ...
