a8619d019c3edb74d9ea991cf515296856856c63
iKiray <exkiray@gmail.com>
2025-12-03 22:25:04 -0500

    Added div with head and para inside, with style

--- domManipulationAndEvents/script.js
+++ domManipulationAndEvents/script.js
@@ -10,3 +10,17 @@ const blueHead = document.createElement("h3");
 blueHead.textContent = "I'm a blue h3!";
 blueHead.style.color = "blue";
 container.appendChild(blueHead);
+
+const div = document.createElement("div");
+div.style.border = "2px solid black";
+div.style.backgroundColor = "pink";
+
+const head = document.createElement("h1");
+head.textContent = "I'm in a div";
+div.appendChild(head);
+
+const para = document.createElement("p");
+para.textContent = "ME TOO!";
+div.appendChild(para);
+
+container.appendChild(div);
